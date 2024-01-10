c959333f8bba669870a824fb794f91a89c87c04c
nicholas_lambiase <nicholaslambiase14@gmail.com>
2024-01-10 13:54:00 -0600

    Implemented game function plays five rounds of rock paper scissors prompting the user then displays the final result of who won best of 5 games

--- script.js
+++ script.js
@@ -12,8 +12,6 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection){
-    //ensure playerselection is lowercase
-    playerSelection = playerSelection.toLowerCase();
 
     let inputComparisionMssg = `${playerSelection} vs ${computerSelection}\n`;
     let winMessage = 'You Win! Congrats!!!\n';
@@ -28,37 +26,37 @@ function playRound(playerSelection, computerSelection){
 
         //Immediatly return if there is a tie
         if (playerSelection === computerSelection) {
-            console.log(tieMessage);
-            return 2;
+            console.log(tieMessage + '\n');
+            return -1;
         }
 
         if (playerSelection === 'rock') {
             if (computerSelection === 'paper') {
-                console.log(loseMessage);
+                console.log(loseMessage + '\n');
                 return 0;
             }
             else {
-                console.log(winMessage);
+                console.log(winMessage + '\n');
                 return 1;
             }       
         }
         else if (playerSelection === 'paper') {
             if (computerSelection === 'scissors') {
-                console.log(loseMessage);
+                console.log(loseMessage + '\n');
                 return 0;
             }
             else {
-                console.log(winMessage);
+                console.log(winMessage + '\n');
                 return 1;
             }       
         }
         else if (playerSelection === 'scissors') {
             if (computerSelection === 'rock') {
-                console.log(loseMessage);
+                console.log(loseMessage + '\n');
                 return 0;
             }
             else {
-                console.log(winMessage);
+                console.log(winMessage + '\n');
                 return 1;
             }       
         }
@@ -68,18 +66,32 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+function getUserInput() {
+    let validInput = false;
+    while (!validInput) {
+        playerSelection = prompt('Please select Rock, Paper, or Scissors');
+        playerSelection = playerSelection.toLowerCase();
+        if (playerSelection !== 'rock' | playerSelection !== 'paper' | playerSelection !== 'scissors') {
+            validInput = true;
+            return playerSelection;
+        }
+        else
+            alert('Please enter a valid choice');
+    }
+}
+
 function game() {
+    let validInput
     let playerSelection
     let computerSelection
     let roundResult
+    let playerTally = 0;
+    let computerTally = 0;
     
     //Loops through 5 rounds of rock paper scissors
     for (let index = 0; index < 5; index++) {
 
-        //Asks the user for an input until it is deemed valid
-        do {
-            playerSelection = prompt('Please select Rock, Paper, or Scissors');
-        } while ((playerSelection.toLowerCase() != 'rock' | playerSelection.toLowerCase() != 'paper' | playerSelection.toLowerCase() != 'scissors'));
+        playerSelection = getUserInput();
 
         computerSelection = getComputerChoice();
 
@@ -88,10 +100,29 @@ function game() {
         //Scoring logic...
         /*  0 = player loss
             1 = player win
-            2 = tie
+            -1 = tie
         */
+
+        if (roundResult === 0)
+            computerTally++;
+        else if (roundResult === 1)
+            playerTally++;
+        else {
+            //if there is a tie we need to replay the round
+            index--;
+        }
+
     }
 
     //Final message logic
+    if (playerTally > computerTally)
+        console.log('Congrats! You won against the Computer');
+    else if (computerTally > playerTally)
+        console.log('Uh oh... You lost to the Computer :(');
+    else
+        console.log('Wow! it was a tie! That\'s rare!');
 
 }
+
+//Main execution
+game();
