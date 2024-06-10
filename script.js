bcf354aade611dc059234aa9fc586088b9d4c4cc
nicholas_lambiase <nicholaslambiase14@gmail.com>
2024-06-09 23:00:18 -0500

    Finished the RPS UI

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
 
+const selectionButtons = document.querySelectorAll(".selection-btn");
+selectionButtons.forEach((button) => {
+    button.addEventListener("click", function (e){
+        let playerSelection = button.id;
+        game(playerSelection);
+    });
+});
+
+function game(playerSelection) {
+    let computerSelection
+    let roundResult
+
+    computerSelection = getComputerChoice();
+    roundResult = playRound(playerSelection, computerSelection);
+
+    let inputComparisionMssg = `${playerSelection} vs ${computerSelection}\n`;
+    let winMessage = 'You Win! Congrats!!!\n';
+    let tieMessage = 'Oh man, that was a close one... It\'s a tie\n';
+    let loseMessage = 'You lose :(\n';
+
+    let result_span = document.querySelector("#game-result-span");
+    
+    let result_text = document.createElement("p");
+    result_text.id = "result-text";
+
+    let input_comparison = document.createElement("h3");
+    input_comparison.id = "comp-text";
+    input_comparison.textContent = inputComparisionMssg;
+
+    if (document.querySelector("#comp-text"))
+        document.querySelector("#comp-text").remove();
+
+    if (document.querySelector("#result-text"))
+        document.querySelector("#result-text").remove();
+
+    //Final message logic
+    if (roundResult == 1)
+        result_text.textContent = winMessage;
+    else if (roundResult == 0)
+        result_text.textContent = loseMessage;
+    else
+        result_text.textContent = tieMessage;
+
+        result_span.appendChild(input_comparison);
+        result_span.appendChild(result_text);
+}
+
 function getComputerChoice(){
     choiceNum = Math.floor(Math.random()*3);
     
@@ -18,113 +65,45 @@ function playRound(playerSelection, computerSelection){
     let winMessage = 'You Win! Congrats!!!\n';
     let tieMessage = 'Oh man, that was a close one... It\'s a tie\n';
     let loseMessage = 'You lose :(\n';
-
-    //validate user input
-    if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
-        
-        // Show the user the compared inputs
-        console.log(inputComparisionMssg);
-
-        //Immediatly return if there is a tie
-        if (playerSelection === computerSelection) {
-            console.log(tieMessage + '\n');
-            return -1;
-        }
-
-        // Determine the result based on the player and computer choices
-        if (playerSelection === 'rock') {
-            if (computerSelection === 'paper') {
-                console.log(loseMessage + '\n');
-                return 0;
-            }
-            else {
-                console.log(winMessage + '\n');
-                return 1;
-            }       
-        }
-        else if (playerSelection === 'paper') {
-            if (computerSelection === 'scissors') {
-                console.log(loseMessage + '\n');
-                return 0;
-            }
-            else {
-                console.log(winMessage + '\n');
-                return 1;
-            }       
-        }
-        else if (playerSelection === 'scissors') {
-            if (computerSelection === 'rock') {
-                console.log(loseMessage + '\n');
-                return 0;
-            }
-            else {
-                console.log(winMessage + '\n');
-                return 1;
-            }       
-        }
-    }
-    else {
-        return 'Please select Rock, Paper, or Scissors\n';
-    }
-}
-
-function getUserInput() {
-    let validInput = false;
-    while (!validInput) {
-        playerSelection = prompt('Please select Rock, Paper, or Scissors');
-        playerSelection = playerSelection.toLowerCase();
-        if (playerSelection === 'rock' | playerSelection === 'paper' | playerSelection === 'scissors') {
-            validInput = true;
-            return playerSelection;
-        }
-        else
-            alert('Please enter a valid choice');
-    }
-}
-
-function game() {
-    let validInput
-    let playerSelection
-    let computerSelection
-    let roundResult
-    let playerTally = 0;
-    let computerTally = 0;
     
-    //Loops through 5 rounds of rock paper scissors
-    for (let index = 0; index < 5; index++) {
+    // Show the user the compared inputs
+    console.log(inputComparisionMssg);
 
-        playerSelection = getUserInput();
-
-        computerSelection = getComputerChoice();
-
-        roundResult = playRound(playerSelection, computerSelection);
-
-        //Scoring logic...
-        /*  0 = player loss
-            1 = player win
-            -1 = tie
-        */
-
-        if (roundResult === 0)
-            computerTally++;
-        else if (roundResult === 1)
-            playerTally++;
-        else {
-            //if there is a tie we need to replay the round
-            index--;
-        }
-
+    //Immediatly return if there is a tie
+    if (playerSelection === computerSelection) {
+        console.log(tieMessage + '\n');
+        return -1;
     }
 
-    //Final message logic
-    if (playerTally > computerTally)
-        console.log('Congrats! You won against the Computer');
-    else if (computerTally > playerTally)
-        console.log('Uh oh... You lost to the Computer :(');
-    else
-        console.log('Wow! it was a tie! That\'s rare!');
-
+    // Determine the result based on the player and computer choices
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            console.log(loseMessage + '\n');
+            return 0;
+        }
+        else {
+            console.log(winMessage + '\n');
+            return 1;
+        }       
+    }
+    else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            console.log(loseMessage + '\n');
+            return 0;
+        }
+        else {
+            console.log(winMessage + '\n');
+            return 1;
+        }       
+    }
+    else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            console.log(loseMessage + '\n');
+            return 0;
+        }
+        else {
+            console.log(winMessage + '\n');
+            return 1;
+        }       
+    }
 }
-
-//Main execution
-game();
